fb04a4b29ac0c5052b09b5f0c4879b791402d0b9
Joshua Herron <herronjo110@live.com>
2016-10-08 15:46:31 -0400

    Update setup_linux.sh

--- setup_linux.sh
+++ setup_linux.sh
@@ -1,4 +1,2 @@
-mv FreeBASIC* FreeBASIC.tar.gz;
-tar -xvzf FreeBASIC.tar.gz;
-mv FreeBASIC* FreeBASIC;
-./FreeBASIC/bin/fbc krnl_linux.bas;
+tar -xvzf FreeBASIC-1.05.0-linux-x86_64.tar.gz;
+./FreeBASIC-0.90.1-linux/bin/fbc krnl_linux.bas;
