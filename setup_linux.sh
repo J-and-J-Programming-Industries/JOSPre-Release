594d6ffa1d414c0b7fc6b149c45317a18e4a5d95
Joshua Herron <herronjo110@live.com>
2016-10-08 15:01:27 -0400

    Update setup_linux.sh

--- setup_linux.sh
+++ setup_linux.sh
@@ -1,5 +1,4 @@
-wget "http://downloads.sourceforge.net/project/fbc/Binaries%20-%20Linux/FreeBASIC-1.05.0-linux-x86.tar.gz?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Ffbc%2Ffiles%2FBinaries%2520-%2520Linux%2F&ts=1475949992&use_mirror=pilotfiber";
 mv FreeBASIC* FreeBASIC.tar.gz;
 tar -xvzf FreeBASIC.tar.gz;
-mv FreeBASIC-1.05.0-linux-x86 FreeBASIC;
+mv FreeBASIC* FreeBASIC;
 ./FreeBASIC/bin/fbc krnl_linux.bas;
